7d0f8ccbef3eb33ec25acd4927629cf2c6cfdb3b
cemvarol <cem.varol@outlook.com>
2021-05-21 16:25:42 +0100

    Update ChromeandOfficeDL.ps1

--- ChromeandOfficeDL.ps1
+++ ChromeandOfficeDL.ps1
@@ -10,5 +10,7 @@ $output2 = "$env:USERPROFILE\desktop\Office2016.url"
 Invoke-WebRequest -Uri $url2 -OutFile $output2
 
 #& "C:\Program Files\google\chrome\Application\chrome.exe" $env:USERPROFILE\desktop\Office2016.url
+Start-Sleep -s 40
+
 
 & "C:\Program Files\google\chrome\Application\chrome.exe" https://officecdn.microsoft.com/db/492350F6-3A01-4F97-B9C0-C7C6DDF67D60/media/en-US/Excel2019Retail.img
