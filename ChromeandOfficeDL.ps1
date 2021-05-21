93edcd66c32928e72ae61c57156bf0c5dcadd9ce
cemvarol <cem.varol@outlook.com>
2021-05-21 16:22:08 +0100

    Update ChromeandOfficeDL.ps1

--- ChromeandOfficeDL.ps1
+++ ChromeandOfficeDL.ps1
@@ -1,20 +1,14 @@
 
-$url = "https://github.com/cemvarol/AZ-Migration/blob/master/ChromeSetup.exe?raw=true"
-$output = "$env:USERPROFILE\downloads\ChromeSetup.exe"
-Invoke-WebRequest -Uri $url -OutFile $output
+$url1 = "https://github.com/cemvarol/AZ-Migration/blob/master/ChromeSetup.exe?raw=true"
+$output1 = "$env:USERPROFILE\downloads\ChromeSetup.exe"
+Invoke-WebRequest -Uri $url1 -OutFile $output1
 
 & "$env:USERPROFILE\downloads\ChromeSetup.exe"
 
-$url = "https://raw.githubusercontent.com/cemvarol/DC/master/Office2016.url"
-$output1 = "$env:USERPROFILE\desktop\Office2016.url"
-Invoke-WebRequest -Uri $url -OutFile $output1
-
-
-& "$env:USERPROFILE\downloads\ChromeSetup.exe"
-
-
+$url2 = "https://raw.githubusercontent.com/cemvarol/DC/master/Office2016.url"
+$output2 = "$env:USERPROFILE\desktop\Office2016.url"
+Invoke-WebRequest -Uri $url2 -OutFile $output2
 
 #& "C:\Program Files\google\chrome\Application\chrome.exe" $env:USERPROFILE\desktop\Office2016.url
-
 
 "C:\Program Files\google\chrome\Application\chrome.exe" https://officecdn.microsoft.com/db/492350F6-3A01-4F97-B9C0-C7C6DDF67D60/media/en-US/Excel2019Retail.img
